79f47960f9a4ab7bad3904cbd3e852a5aeaff37d
Nhat Ha <nhathadt11@gmail.com>
2018-10-08 19:53:01 +0700

    Initial commit

---
+++ src/main/java/study/cloudcomputing/courseapp/CourseAppApplication.java
@@ -0,0 +1,12 @@
+package study.cloudcomputing.courseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAppApplication.class, args);
+	}
+}
